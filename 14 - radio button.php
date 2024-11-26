1c8fd078b3d99cf49f0f5dfb8037003b354f5c91
Pixyz-bit <kaiphoenix48@gmail.com>
2024-11-27 00:29:08 +0800

    Update 14 - radio button.php

--- 14 - radio button.php
+++ 14 - radio button.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <form action="14 - radio button.php" method="post">
-        <input type="radio" value="Visa" name="credit_card"> Visa <br>
+        <input type="radio" value="Visa" name="credit_card"> Visa <br> <!--Name attribute to prevent multiple ticks on checkboxes-->
         <input type="radio" value="Mastercard" name="credit_card"> Mastercard <br>
         <input type="radio" value="American Express" name="credit_card"> American Express <br>
         <input type="submit" name="confirm" value = "confirm">
@@ -15,10 +15,25 @@
 </body>
 </html>
 <?php
-    if(isset($_POST["confirm"])){
-        $credit_card = $_POST["credit_card"];
-        echo $credit_card;
+    if(isset($_POST["confirm"])){ //$_POST[Name Attribute]
+        $credit_card = null;
 
-        if
+        if(isset($_POST["credit_card"])){
+            $credit_card = $_POST["credit_card"];
+        }
+        
+        switch($credit_card){
+            case "Visa": //case "value attribute": 
+                echo "You selected Visa";
+                break;
+            case "Mastercard":
+                echo "You selected Mastercard";
+                break;
+            case "American Express":
+                echo "You selected American Express";
+                break;
+            default: 
+                echo "Please make a selection";
+        }
     }
 ?>
